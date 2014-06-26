639533becbce39d7a40564c20f23cdd3d1813c66
Éber Freitas Dias <eber.freitas@gmail.com>
2014-06-26 04:54:31 -0300

    Trying to fix tests on Travis

--- Test/Case/Model/Behavior/SlugomaticBehaviorTest.php
+++ Test/Case/Model/Behavior/SlugomaticBehaviorTest.php
@@ -3,7 +3,7 @@
 App::uses('Model', 'Model');
 App::uses('AppModel', 'Model');
 
-require_once CAKE . 'Test' . DS . 'CASE' . DS . 'Model' . DS . 'models.php';
+require_once CAKE . 'Test' . DS . 'Case' . DS . 'Model' . DS . 'models.php';
 
 class SlugomaticBehaviorTest extends CakeTestCase {
 
